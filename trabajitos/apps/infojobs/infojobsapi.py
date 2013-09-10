4ad783cb6efd4405a5af10e4393138148efc685f
drenedo <drenedo@gmail.com>
2013-09-10 17:47:01 +0200

    API

--- trabajitos/apps/infojobs/infojobsapi.py
+++ trabajitos/apps/infojobs/infojobsapi.py
@@ -1,10 +1,10 @@
 from django.conf import settings
 
 import logging
-
 import urllib
 import urllib2
 
+from infojobs import InfojobsJob
 import simplejson as json
 
 logger = logging.getLogger('schedule')
@@ -14,26 +14,54 @@ class InfojobsSearchAPI:
     def __init__(self, key=None, provlist=None):
         self.key = key
         self.provlist = provlist
+        self.petition = 1
+        self.total = 1
 
     def find(self):
         joblist = []
+	joblist = self.getPetition(self.petition)
+
+        while self.petition > 1 and self.total > 1 and self.petition <= self.total:
+            joblist.extend(self.getPetition(self.petition))
+            
+            
+
+    def getPetition(self,num):
         passman = urllib2.HTTPPasswordMgrWithDefaultRealm()
         passman.add_password(None, TRIM_API_URL, settings.INFOJOBSID, settings.INFOJOBSCR)
         authhandler = urllib2.HTTPBasicAuthHandler(passman)
         opener = urllib2.build_opener(authhandler)
         urllib2.install_opener(opener)
-	url = TRIM_API_URL+'/1/offer?'
-	if self.provlist:
+        url = TRIM_API_URL+'/1/offer?'
+        if self.provlist:
             for pro in self.provlist:
                 url = url+'province='+pro+'&'
 
         if self.key:
-            url = url+'q'+self.key
-	
-	
-        response = urllib2.urlopen(url)
-        url = response.read().strip()
-        objects = json.loads(url)
+            url = url+'q='+self.key
 
-        logger.debug(objects['offers'].__len__())     
- 
+
+        response = urllib2.urlopen(url)
+        content = response.read().strip()
+        objects = json.loads(content)
+
+        logger.debug("Load::"+url)
+        logger.debug(objects['offers'].__len__())
+
+	self.petition = self.petition + 1
+	self.total = objects['totalPages']
+
+        return self.getObjects(objects['offers'])
+
+    def getObjects(self,objects):
+        joblist=[]
+        if objects:
+            for object in objects:
+                ifj = InfojobsJob(object['link'],object['title'],object['author']['name'],object['study']['value']+' '+object['requirementMin'])
+                logger.debug("Link::"+ifj.title)
+                logger.debug("Load::"+ifj.href)
+                logger.debug("Load::"+ifj.company)
+                logger.debug("Load::"+ifj.description)
+                if(ifj.href!=None):
+                    joblist.append(ifj)
+        return joblist
